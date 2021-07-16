fa37675c5b2c7d357a9f21d85ba93cc9ae3763bb
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-07-15 21:47:33 -0700

    added map js

---
+++ Mass_Shootings/static/js/map.js
@@ -0,0 +1,10 @@
+var map = L.map("map", {
+    center: [0, 0],
+    zoom: 2,
+  }); 
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+    }).addTo(map);
