518ae3b46c27608275c3b2d4ffec903b204fb4a1
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-07-18 17:58:29 -0700

    work in progress

--- Mass_Shootings/static/js/axes.js
+++ Mass_Shootings/static/js/axes.js
@@ -22,66 +22,101 @@ function a() {
 
     // Append the scatter group and shift the group by left and top margins
   var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`)
-    .attr("class", "barchart")
-    
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);  
+
+  var chosenXAxis = "race";
+  var chosenYAxis = "count";
+
+  //--- X-SCALE function - updates xScale upon axis change ---//
+  function xScale(xdata, chosenXAxis) {
+    var xBandScale = d3.scaleBand()
+      .domain(xdata.map(d => d[chosenXAxis]))
+      .range([0, width])
+      .padding(0.1);
+    // create scales
+    return xBandScale;
+  }
+  //--- Y-SCALE function - updates yScale upon axis change ---//
+  function yScale(ydata, chosenYAxis) {
+    //create scales
+    var yLinearScale = d3.scaleLinear()
+      .domain([ d3.min(ydata, d => d[chosenYAxis] - 2),
+                d3.max(ydata, d => d[chosenYAxis]) + 2 ])
+      .range([height, 0]);
+    return yLinearScale;
+  }
+  //--- X-AXIS RENDER function - updates bottom axis upon axis change ---//
+  function renderXaxis(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
+    return xAxis;
+  }
+  //--- Y-AXIS RENDER function - updates left axis upon axis change ---//
+  function renderYaxis(newYScale, yAxis) {
+    var leftAxis = d3.axisLeft(newYScale);
+    yAxis.transition()
+      .duration(1000)
+      .call(leftAxis);
+    return yAxis;
+  }
+  
   d3.json("/api/mass_shootings").then((massData, err) => {
     if (err) throw err;
-      console.log(massData);
-    
-      // Get race incidences count
+    console.log(massData);
+  
+    // Get race incidences count
     var allRaces = [];
-    var eachRace = {};
-
     var races = massData.map(d => d.race);
-    console.log(races);
 
-    eachRace = eachRace["White"] = races.filter((v) => (v === "White")).length;
-    allRaces.push(eachRace);
-    eachRace = eachRace["Black"] = races.filter((v) => (v === "Black")).length;
-    allRaces.push(eachRace);
-
-    eachRace["Latino"] = races.filter((v) => (v === "Latino")).length;
-    eachRace["Native American"] = races.filter((v) => (v === "Native American")).length;
-    eachRace["Asian"] = races.filter((v) => (v === "Asian")).length;
-    eachRace["Other"] = races.filter((v) => (v === "Other")).length;
-    eachRace["Unknown"] = races.filter((v) => (v === "Unknown")).length;
-    eachRace["Unclear"] = races.filter((v) => (v === "Unclear")).length;
-
-    // allRaces.push(races.filter((v) => (v === "White")).length);
-    // allRaces.push(races.filter((v) => (v === "Black")).length);
-    // allRaces.push(races.filter((v) => (v === "Latino")).length);
-    // allRaces.push(races.filter((v) => (v === "Native American")).length);
-    // allRaces.push(races.filter((v) => (v === "Asian")).length);
-    // allRaces.push(races.filter((v) => (v === "Other")).length);
-    // allRaces.push(races.filter((v) => (v === "Unknown")).length);
-    // allRaces.push(races.filter((v) => (v === "Unknown")).length);
-
-    // allRaces["White"] = races.filter((v) => (v === "White")).length;
-    // allRaces["Black"] = races.filter((v) => (v === "Black")).length;
-    // allRaces["Latino"] = races.filter((v) => (v === "Latino")).length;
-    // allRaces["Native American"] = races.filter((v) => (v === "Native American")).length;
-    // allRaces["Asian"] = races.filter((v) => (v === "Asian")).length;
-    // allRaces["Other"] = races.filter((v) => (v === "Other")).length;
-    // allRaces["Unknown"] = races.filter((v) => (v === "Unknown")).length;
-    // allRaces["Unclear"] = races.filter((v) => (v === "Unknown")).length;
+    var cnt = races.filter((v) => (v === "White")).length;
+    allRaces.push({race:"White", count: cnt});
+    cnt = races.filter((v) => (v === "Black")).length;
+    allRaces.push({race:"Black", count: cnt});
+    cnt = races.filter((v) => (v === "Latino")).length;
+    allRaces.push({race:"Latino", count: cnt});   
+    cnt = races.filter((v) => (v === "Native American")).length;
+    allRaces.push({race:"Native American", count: cnt});    
+    cnt = races.filter((v) => (v === "Asian")).length;
+    allRaces.push({race:"Asian", count: cnt});    
+    cnt = races.filter((v) => (v === "Unknown")).length;
+    allRaces.push({race:"Unknown", count: cnt});    
+    cnt = races.filter((v) => (v === "Other")).length;
+    allRaces.push({race:"Other", count: cnt});    
+    cnt = races.filter((v) => (v === "Unclear")).length;
+    allRaces.push({race:"Unclear", count: cnt});    
     
+    allRaces.sort(({count:a}, {count:b}) => b-a);
     console.log(allRaces);
 
-    // console.log(Object.keys(allRaces));
-    // console.log(Object.values(allRaces));
+    // Get age incidences count
+    var allAges = [];
+    var ages = massData.map(d => d.age_of_shooter);
+
+    var cnt = ages.filter((v) => (v >= 10 && v < 20)).length;
+    allAges.push({agebin:"10-19", count: cnt});
+    var cnt = ages.filter((v) => (v >= 20 && v < 30)).length;
+    allAges.push({agebin:"20-29", count: cnt});
+    var cnt = ages.filter((v) => (v >= 30 && v < 40)).length;
+    allAges.push({agebin:"30-39", count: cnt});
+    var cnt = ages.filter((v) => (v >= 40 && v < 50)).length;
+    allAges.push({agebin:"40-49", count: cnt});
+    var cnt = ages.filter((v) => (v >= 50 && v < 60)).length;
+    allAges.push({agebin:"50-59", count: cnt});
+    var cnt = ages.filter((v) => (v >= 60 && v < 70)).length;
+    allAges.push({agebin:"60-69", count: cnt});
+    var cnt = ages.filter((v) => (v == "Unknown")).length;
+    allAges.push({agebin:"Unknown", count: cnt});
+    
+    allAges.sort(({agebin:a}, {agebin:b}) => b-a);
+    console.log(allAges);
     
     // Create x axis scale with padding
-    var xBandScale = d3.scaleBand()
-    .domain(allRaces.map(d => d.keys()))
-    .range([0, width])
-    .padding(0.1);
-
-    // Create a linear scale for the vertical axis.
-    var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(allRaces, d => d.values())])
-    .range([height, 0]);
-
+    var xBandScale = xScale(allRaces, chosenXAxis);
+    // Create a linear scale for the vertical axis
+    var yLinearScale = yScale(allRaces, chosenYAxis);
+    
     // Create two new functions passing our scales in as arguments
     // These will be used to create the chart's axes
     var bottomAxis = d3.axisBottom(xBandScale);
@@ -89,25 +124,84 @@ function a() {
 
     // Append two SVG group elements to the chartGroup area,
     // and create the bottom and left axes inside of them
-    chartGroup.append("g")
-      .call(leftAxis);
-
-    chartGroup.append("g")
+    var xAxis = chartGroup.append("g")
       .attr("transform", `translate(0, ${height})`)
       .call(bottomAxis);
+  
+    var yAxis = chartGroup.append("g")
+      .call(leftAxis);
 
-    // Create one SVG rectangle per piece of tvData
-    // Use the linear and band scales to position each rectangle within the chart
-    chartGroup.select(".barchart").selectAll("rect")
+    // Create one rectagle group - linear and band scales to position each rect in the chart
+    var rectangleGroup = chartGroup.selectAll("rect")
       .data(allRaces)
       .enter()
       .append("rect")
       .attr("class", "bar")
-      .attr("x", d => xBandScale(d))
-      .attr("y", d => yLinearScale(d))
+      .attr("x", d => xBandScale(d.race))
+      .attr("y", d => yLinearScale(d.count))
       .attr("width", xBandScale.bandwidth())
-      .attr("height", d => height - yLinearScale(d));
-    
+      .attr("height", d => height - yLinearScale(d.count))
+
+    // Create x axis label
+    var xLabelGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 10})`)    
+    var raceLabel = xLabelGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 20)
+      .attr("value", "race") // value to grab for event listener
+      .classed("active", true)
+      .attr("class", "axisText")
+      .text("Shooter Race");
+    var agebinLabel = xLabelGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 40)
+      .attr("value", "agebin") // value to grab for event listener 
+      .classed("inactive", true)
+      .text("Shooter Age");
+
+    // Create y axis label
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Number of Incidences");
+
+    // x axis event listener
+    xLabelGroup.selectAll("text").on("click", function() {
+      // get value of selection
+      var value = d3.select(this).attr("value");
+      console.log(value);
+
+      if (value !== chosenXAxis) {
+        chosenXAxis = value;
+        console.log(chosenXAxis);
+        if (chosenXAxis == "race") {
+          // update xScale and axis
+          xBandScale = xScale(allRaces, chosenXAxis);
+          xAxis = renderXaxis(xBandScale, xAxis);
+          // update circles with new x values
+          //circlesGroup = renderXCircles(circlesGroup, xLinearScale, chosenXAxis);
+          // update circles text with new x values
+          //circlesText = renderXtext(circlesText, xLinearScale, chosenXAxis);
+          // updates tooltips with new info
+          //circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
+        }
+        else {
+          // update xScale and axis
+          xBandScale = xScale(allAges, chosenXAxis);
+          xAxis = renderYaxis(xBandScale, xAxis);
+          // update circles with new x values
+          //circlesGroup = renderXCircles(circlesGroup, xLinearScale, chosenXAxis);
+          // update circles text with new x values
+          //circlesText = renderXtext(circlesText, xLinearScale, chosenXAxis);
+          // updates tooltips with new info
+          //circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
+        }
+      }
+    })
+
   });
 } 
 
@@ -115,6 +209,5 @@ function b() {
         $('bcontent').empty();
         d3.select('bcontent')
             .append('p')
-            .text("This is B test")
+            .text("This is B test");
 } 
-
