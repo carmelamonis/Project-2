67c4eb92ccc937b136022f83d015b6e8fcb93500
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-07-19 22:09:25 -0700

    tooltip

--- Mass_Shootings/static/js/axes.js
+++ Mass_Shootings/static/js/axes.js
@@ -6,7 +6,7 @@ var svgHeight = 700;
 
 // Set margins for bar
 var margin = { top: 20, right: 40, bottom: 90, left: 100 };
-  
+
 // Define bar width and height (svg area - margins)
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
@@ -26,21 +26,61 @@ var chartGroup = svg.append("g")
 var chosenXAxis = "race";
 var chosenYAxis = "count";
 
-  //--- X SCALE function - updates xScale upon axis change ---//
+//--- RACE VICTIM aggregate function ---//
+function raceVictimTotals(massData, race) {
+  var temp = [];
+  var c1 = 0;
+  var c2 = 0;
+  var c3 = 0;
+  massData.forEach(d => {
+    if (d.race == race) {
+      c1 += d.total_victims;
+      c2 += d.fatalities;
+      c3 += d.injured; 
+    }
+  })
+  temp.push(c1, c2, c3);
+  return temp;
+}
+//--- AGEBIN VICTIM aggregate function ---//
+function ageVictimTotals(massData, young, old) {
+  var temp = [];
+  var c1 = 0;
+  var c2 = 0;
+  var c3 = 0;
+  massData.forEach(d => {
+    if (d.age_of_shooter >= young && d.age_of_shooter < old) {
+      c1 += d.total_victims;
+      c2 += d.fatalities;
+      c3 += d.injured; 
+    }
+  })
+  temp.push(c1, c2, c3);
+  return temp;
+}//--- X SCALE function - updates xScale upon axis change ---//
 function xScale(xdata, chosenXAxis) {
   var xBandScale = d3.scaleBand()
     .domain(xdata.map(d => d[chosenXAxis]))
     .range([0, width])
-    .padding(0.15);
+    .padding(0.1);
   return xBandScale;
 }
 //--- Y SCALE function - updates yScale upon axis change ---//
 function yScale(ydata, chosenYAxis) {
-  var yLinearScale = d3.scaleLinear()
-    .domain([ d3.min(ydata, d => d[chosenYAxis] - 2),
-              d3.max(ydata, d => d[chosenYAxis] + 2 ) ])
+  if (chosenYAxis == "total_vic") {
+    var yLinearScale = d3.scaleLinear()
+    .domain([ d3.min(ydata, d => d[chosenYAxis] - 88 ),
+              d3.max(ydata, d => d[chosenYAxis] + 88 ) ])
     .range([height, 0]);
   return yLinearScale;
+  }
+  else {
+    var yLinearScale = d3.scaleLinear()
+    .domain([ d3.min(ydata, d => d[chosenYAxis] - 4 ),
+              d3.max(ydata, d => d[chosenYAxis] + 4 ) ])
+    .range([height, 0]);
+  return yLinearScale;
+  }
 }
 //--- X AXIS RENDER function - updates bottom axis upon axis change ---//
 function renderXaxis(newXScale, xAxis) {
@@ -60,44 +100,98 @@ function renderYaxis(newYScale, yAxis) {
 }
 //--- RECTANGLE RENDER function - updates rectangle group upon axis change ---//
 function renderRect(newData, newXScale, chosenXAxis, newYScale, chosenYAxis) {
-  chartGroup.selectAll("rect").remove()
+  var xlabel;
+    if (chosenXAxis === "race") {
+      xlabel = "Race: ";
+    }
+    else {
+      xlabel = "Age Group: ";
+    }
+    var ylabel;
+    if (chosenYAxis === "count") {
+      ylabel = "# Incidents: ";
+    }
+    else {
+      ylabel = "Total Victims: ";
+    }
+  // create a tooltip
+  var Tooltip = d3.select("#bar")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "4px")
+    .style("padding", "4px")
+    
+  // Two functions that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip
+      .style("opacity", 1) // makes tooltip show
+      .style("left", (d3.mouse(this)[0] + 100) + "px")
+      .style('top', (height + margin.top + 20) + 'px')
+      //.style("top", (d3.mouse(this)[1]) + "px")
+      .html(`${xlabel}${d[chosenXAxis]}<br>${ylabel}${d[chosenYAxis]}`)
+    d3.select(this)
+      .style("stroke", "black")
+      .style("fill", "green")  
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .style("stroke", "none")
+      .style("fill", "#c3073F")  
+  }
+  chartGroup.selectAll("rect").remove() 
+  // Create one rectagle group - linear and band scales to position each rect in the chart
   var rectangleGroup = chartGroup.selectAll("rect")
     .data(newData)
     .enter()
     .append("rect")
-    .attr("class", "bar")
-    .attr("x", d => newXScale(d[chosenXAxis]))
-    .attr("y", d => newYScale(d[chosenYAxis]))
-    .attr("width", newXScale.bandwidth())
-    .attr("height", d => height - newYScale(d[chosenYAxis]));
+      .attr("class", "bar")
+      .attr("x", d => newXScale(d[chosenXAxis]))
+      .attr("y", d => newYScale(d[chosenYAxis]))
+      .attr("width", newXScale.bandwidth())
+      .attr("height", d => height - newYScale(d[chosenYAxis]))
+      .attr("fill", "#c3073F")
+    .on("mouseover", mouseover)
+    .on("mouseleave", mouseleave);
   return rectangleGroup;
-}  
+}
 
 d3.json("/api/mass_shootings").then((massData, err) => {
   if (err) throw err;
   console.log(massData);
-  // Get race incidences count
+  
+  // Get race incidences count and victims
   var allRaces = [];
   var races = massData.map(d => d.race);
-  var victims = massData.map(d => d.total_victims);
 
   var cnt = races.filter(v => (v === "White")).length;
-  var vic_cnt = victims.filter(v => v.total_victims).length;
-  allRaces.push({race:"White", count: cnt, victims: vic_cnt});
+  var holder = raceVictimTotals(massData, "White");
+  allRaces.push({race:"White", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]}); 
   cnt = races.filter(v => (v === "Black")).length;
-  allRaces.push({race:"Black", count: cnt});
+  var holder = raceVictimTotals(massData, "Black");
+  allRaces.push({race:"Black", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});
   cnt = races.filter(v => (v === "Latino")).length;
-  allRaces.push({race:"Latino", count: cnt});   
+  var holder = raceVictimTotals(massData, "Latino");
+  allRaces.push({race:"Latino", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});   
   cnt = races.filter(v => (v === "Native American")).length;
-  allRaces.push({race:"Native American", count: cnt});    
+  var holder = raceVictimTotals(massData, "Native American");
+  allRaces.push({race:"Native American", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});    
   cnt = races.filter(v => (v === "Asian")).length;
-  allRaces.push({race:"Asian", count: cnt});    
+  var holder = raceVictimTotals(massData, "Asian");
+  allRaces.push({race:"Asian", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});    
   cnt = races.filter(v => (v === "Unknown")).length;
-  allRaces.push({race:"Unknown", count: cnt});    
+  var holder = raceVictimTotals(massData, "Unknown");
+  allRaces.push({race:"Unknown", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});    
   cnt = races.filter(v => (v === "Other")).length;
-  allRaces.push({race:"Other", count: cnt});    
+  var holder = raceVictimTotals(massData, "Other");
+  allRaces.push({race:"Other", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});    
   cnt = races.filter(v => (v === "Unclear")).length;
-  allRaces.push({race:"Unclear", count: cnt});    
+  var holder = raceVictimTotals(massData, "Unclear");
+  allRaces.push({race:"Unclear", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});    
     
   allRaces.sort(({count:a}, {count:b}) => b-a);
   console.log(allRaces);
@@ -107,19 +201,37 @@ d3.json("/api/mass_shootings").then((massData, err) => {
   var ages = massData.map(d => d.age_of_shooter);
 
   var cnt = ages.filter(v => (v >= 10 && v < 20)).length;
-  allAges.push({agebin:"10-19", count: cnt});
+  var holder = ageVictimTotals(massData, 10, 20);
+  allAges.push({agebin:"10-19", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});
   var cnt = ages.filter(v => (v >= 20 && v < 30)).length;
-  allAges.push({agebin:"20-29", count: cnt});
+  var holder = ageVictimTotals(massData, 20, 30);
+  allAges.push({agebin:"20-29", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});
   var cnt = ages.filter(v => (v >= 30 && v < 40)).length;
-  allAges.push({agebin:"30-39", count: cnt});
+  var holder = ageVictimTotals(massData, 30, 40);
+  allAges.push({agebin:"30-39", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});
   var cnt = ages.filter(v => (v >= 40 && v < 50)).length;
-  allAges.push({agebin:"40-49", count: cnt});
+  var holder = ageVictimTotals(massData, 40, 50);
+  allAges.push({agebin:"40-49", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});
   var cnt = ages.filter(v => (v >= 50 && v < 60)).length;
-  allAges.push({agebin:"50-59", count: cnt});
+  var holder = ageVictimTotals(massData, 50, 60);
+  allAges.push({agebin:"50-59", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});
   var cnt = ages.filter(v => (v >= 60 && v < 70)).length;
-  allAges.push({agebin:"60-69", count: cnt});
+  var holder = ageVictimTotals(massData, 60, 70);
+  allAges.push({agebin:"60-69", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});
   var cnt = ages.filter(v => (v == "Unknown")).length;
-  allAges.push({agebin:"Unknown", count: cnt});
+  var holder = [];
+  var c1 = 0;
+  var c2 = 0;
+  var c3 = 0;
+  massData.forEach(d => { 
+    if (d.age_of_shooter == "Unknown") { 
+      c1 += d.total_victims;
+      c2 += d.fatalities;
+      c3 += d.injured; 
+    }
+  })
+  holder.push(c1, c2, c3); 
+  allAges.push({agebin:"Unknown", count: cnt, total_vic: holder[0], fatal: holder[1], injured: holder[2]});
     
   allAges.sort(({agebin:a}, {agebin:b}) => b-a);
   console.log(allAges);
@@ -143,16 +255,49 @@ d3.json("/api/mass_shootings").then((massData, err) => {
   var yAxis = chartGroup.append("g")
     .call(leftAxis);
 
-  // Create one rectagle group - linear and band scales to position each rect in the chart
+  // create a tooltip
+  var Tooltip = d3.select("#bar")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "4px")
+    .style("padding", "4px")
+    
+    
+  // Two functions that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip
+      .style("opacity", 1) // makes tooltip show
+      .style("left", (d3.mouse(this)[0] + 100) + "px")
+      .style('top', (height + margin.top + 20) + 'px')
+      //.style("top", (d3.mouse(this)[1]) + "px")
+      .html(`Race: ${d.race}<br>Incidents: ${d.count}`)
+    d3.select(this)
+      .style("stroke", "black")
+      .style("fill", "green")  
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .style("stroke", "none")
+      .style("fill", "#c3073F")  
+  }
+ // Create one rectagle group - linear and band scales to position each rect in the chart
   var rectangleGroup = chartGroup.selectAll("rect")
     .data(allRaces)
     .enter()
     .append("rect")
-    .attr("class", "bar")
-    .attr("x", d => xBandScale(d.race))
-    .attr("y", d => yLinearScale(d.count))
-    .attr("width", xBandScale.bandwidth())
-    .attr("height", d => height - yLinearScale(d.count));
+      .attr("class", "bar")
+      .attr("x", d => xBandScale(d.race))
+      .attr("y", d => yLinearScale(d.count))
+      .attr("width", xBandScale.bandwidth())
+      .attr("height", d => height - yLinearScale(d.count))
+      .attr("fill", "#c3073F")
+    .on("mouseover", mouseover)
+    .on("mouseleave", mouseleave);
 
   // Create x axis labels
   var xLabelGroup = chartGroup.append("g")
@@ -162,7 +307,6 @@ d3.json("/api/mass_shootings").then((massData, err) => {
     .attr("y", 20)
     .attr("value", "race") // value to grab for event listener
     .classed("active", true)
-    .attr("class", "axisText")
     .text("Shooter Race");
   var agebinLabel = xLabelGroup.append("text")
     .attr("x", 0)
@@ -179,12 +323,12 @@ d3.json("/api/mass_shootings").then((massData, err) => {
     .attr("y", 0 - margin.right)
     .attr("value", "count")
     .classed("active", true)
-    .text("Number of Incidences");
-  var victimLabel = yLabelGroup.append("text")
+    .text("Number of Incidents");
+  var victimsLabel = yLabelGroup.append("text")
     .attr("transform", "rotate(-90)")
     .attr("x", 0 - (height / 2))
     .attr("y", 0 - margin.right - 20)
-    .attr("value", "victim")
+    .attr("value", "total_vic")
     .classed("inactive", true)
     .text("Number of Victims");
   
@@ -192,11 +336,9 @@ d3.json("/api/mass_shootings").then((massData, err) => {
   xLabelGroup.selectAll("text").on("click", function() {
     // get value of selection
     var value = d3.select(this).attr("value");
-    console.log(`Current x: ${chosenXAxis}`);
-    console.log(`New x: ${value}`);
-    
     if (value !== chosenXAxis) {
       chosenXAxis = value;
+      console.log(`Chosen x: ${chosenXAxis}`);
       if (chosenXAxis == "race") {
         // update xScale and axis
         xBandScale = xScale(allRaces, chosenXAxis);
@@ -205,8 +347,6 @@ d3.json("/api/mass_shootings").then((massData, err) => {
         yAxis = renderYaxis(yLinearScale, yAxis);
         // update rectangles with new x values
         rectangleGroup = renderRect(allRaces, xBandScale, chosenXAxis, yLinearScale, chosenYAxis);
-        // updates tooltips with new info
-         //rectangleGroup = updateToolTip(chosenXAxis, chosenYAxis, rectangleGroup);
       }
       if (chosenXAxis == "agebin") {
         // update xScale and axis
@@ -216,20 +356,68 @@ d3.json("/api/mass_shootings").then((massData, err) => {
         yAxis = renderYaxis(yLinearScale, yAxis);
         // update rectangles with new x values
         rectangleGroup = renderRect(allAges, xBandScale, chosenXAxis, yLinearScale, chosenYAxis);
-        // updates tooltips with new info
-         //rectangleGroup = updateToolTip(chosenXAxis, chosenYAxis, rectangleGroup);
       }
     }
-  }) // End xLabelGroup
+    // changes x-axis classes to bold text
+    if (chosenXAxis === "agebin") {
+      agebinLabel
+        .classed("active", true)
+        .classed("inactive", false);
+      raceLabel
+        .classed("active", false)
+        .classed("inactive", true);
+    }
+    if (chosenXAxis === "race") {
+      raceLabel
+        .classed("active", true)
+        .classed("inactive", false);
+      agebinLabel
+        .classed("active", false)
+        .classed("inactive", true);
+    }
+  }); // End xLabelGroup
   // y axis event listener
   yLabelGroup.selectAll("text").on("click", function() {
     // get value of selection
     var value = d3.select(this).attr("value");
-    console.log(`Current y: ${chosenYAxis}`);
-    console.log(`New y: ${value}`);
     if (value !== chosenYAxis) {
       chosenYAxis = value;
+      console.log(`Chosen y: ${chosenYAxis}`);
+      if (chosenXAxis == "race") {
+        // update xScale and axis
+        xBandScale = xScale(allRaces, chosenXAxis);
+        xAxis = renderXaxis(xBandScale, xAxis);
+        yLinearScale = yScale(allRaces, chosenYAxis);
+        yAxis = renderYaxis(yLinearScale, yAxis);
+        // update rectangles with new x values
+        rectangleGroup = renderRect(allRaces, xBandScale, chosenXAxis, yLinearScale, chosenYAxis);
+      }
+      if (chosenXAxis == "agebin") {
+        // update xScale and axis
+        xBandScale = xScale(allAges, chosenXAxis);
+        xAxis = renderXaxis(xBandScale, xAxis);
+        yLinearScale = yScale(allAges, chosenYAxis);
+        yAxis = renderYaxis(yLinearScale, yAxis);
+        // update rectangles with new x values
+        rectangleGroup = renderRect(allAges, xBandScale, chosenXAxis, yLinearScale, chosenYAxis);
+      }
     }   
-  }) // End yLabelGroup
-
+    // changes x-axis classes to bold text
+    if (chosenYAxis === "total_vic") {
+      victimsLabel
+        .classed("active", true)
+        .classed("inactive", false);
+      countLabel
+        .classed("active", false)
+        .classed("inactive", true);
+    }
+    if (chosenYAxis === "count") {
+      countLabel
+        .classed("active", true)
+        .classed("inactive", false);
+      victimsLabel
+        .classed("active", false)
+        .classed("inactive", true);
+    }
+  }); // End yLabelGroup
 }); // End d3.json
