1cc5462d157ab7140d209dd0290efb84f4616d3d
Carmela Monis <carmelamonis@gmail.com>
2021-07-22 17:35:31 -0700

    Cluster map with location layers

--- Mass_Shootings/static/js/map.js
+++ Mass_Shootings/static/js/map.js
@@ -4,39 +4,108 @@ var myMap = L.map("map-id", {
 }); 
 
 function a(){
-  d3.json('/api/mass_shootings').then(function(data) {
-    var markers = L.markerClusterGroup();
-    myMap.eachLayer(function (layer) {
-      myMap.removeLayer(layer);
+  myMap.eachLayer(function (layer) {
+    myMap.removeLayer(layer);
   });
   
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "dark-v10",
-  accessToken: API_KEY
-  }).addTo(myMap);
-
-    //Loop through data
-    for (var i=0; i < data.length; i++) {
-      //console.log(data[i].case);
-      var lat = +data[i].latitude;
-      var lon = +data[i].longitude;
-      var location = [lat, lon];  
-      
-      markers.addLayer(L.marker([lat, lon])
-      .bindPopup("<h5>Case: " + data[i].case + "</h5> <hr>"
-       + "<dd><b>Location:</b> " + data[i].city + ", " + data[i].state + "</dd>"
-       + "<dd><b>Fatalities: </b>" + data[i].fatalities + "</dd>"
-       + "<dd><b>Injured: </b>" + data[i].injured + "</dd>"
-       + "<dd><b>Race of Shooter: </b>" + data[i].race + "</dd>"
-       + "<dd><b>Age of Shooter: </b>" + data[i].age_of_shooter + "</dd>"
-       + "<dd><b>Summary: </b>" + data[i].summary + "</dd>"));
-    }
+  //Load tile layer/base map
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+    }).addTo(myMap);
+  
+  //create cluster group and subgroups
+  var mcg = L.markerClusterGroup(),
+    g1 = L.featureGroup.subGroup(mcg),
+    g2 = L.featureGroup.subGroup(mcg),
+    g3 = L.featureGroup.subGroup(mcg),
+    g4 = L.featureGroup.subGroup(mcg),
+    g5 = L.featureGroup.subGroup(mcg),
+    g6 = L.featureGroup.subGroup(mcg),
+    g7 = L.featureGroup.subGroup(mcg)
+  
+  mcg.addTo(myMap);
+  
+  //Create overlays from subgroups
+  var overlays = {
+    "Workplace": g1,
+    "School": g2,
+    "Military": g3,
+    "Religious": g4,
+    "Airport": g5,
+    "Other": g6,
+    "Unknown" : g7
+  };
+  
+  //Add control to map
+  L.control.layers(null, overlays).addTo(myMap);
+  
+  d3.json('/api/mass_shootings').then(function(data) {
+  
+      //Loop through data
+      for (var i=0; i < data.length; i++) {
+        
+        var location = data[i].location;
+        //console.log(location);
+        var lat = +data[i].latitude;
+        var lon = +data[i].longitude;
+  
+        var newMarker = L.marker([lat, lon]);
+  
+          if (data[i].location == "Workplace") {
+            newMarker.addTo(g1);
+          }
     
-    myMap.addLayer(markers);
-    });
-}
+          else if (data[i].location == "School") {
+            newMarker.addTo(g2);
+          }
+    
+          else if (data[i].location == "Military") {
+            newMarker.addTo(g3);
+          }
+    
+          else if (data[i].location == "Religious") {
+            newMarker.addTo(g4);
+          }
+    
+          else if (data[i].location == "Airport") {
+            newMarker.addTo(g5);
+          }
+    
+          else if (data[i].location == "Other") {
+            newMarker.addTo(g6);
+          }
+    
+          else if (data[i].location == "Unknown") {
+            newMarker.addTo(g7);
+          }
+          
+          //Add the facts in the popup
+          newMarker.bindPopup("<h5>Case: " + data[i].case + "</h5> <hr>"
+           + "<p><b>Location:</b> " + data[i].city + ", " + data[i].state //+ "</dd>"
+           + "<br><b>Date: </b>" + data[i].date //+ "</dd>"
+           + "<br><b>Fatalities: </b>" + data[i].fatalities //+ "</dd>"
+           + "<br><b>Injured: </b>" + data[i].injured// + "</dd>"
+           + "<br><b>Race of Shooter: </b>" + data[i].race// + "</dd>"
+           + "<br><b>Age of Shooter: </b>" + data[i].age_of_shooter// + "</dd>"
+           + "<br><b>Summary: </b>" + data[i].summary// + "</dd>"
+           );
+  
+          } //end of for loop
+      } //end of then
+    ); //end of json
+  
+    g1.addTo(myMap);
+    g2.addTo(myMap);
+    g3.addTo(myMap);
+    g4.addTo(myMap);
+    g5.addTo(myMap);
+    g6.addTo(myMap);
+    g7.addTo(myMap);
+  
+}; //end of function
 
 a()
 
